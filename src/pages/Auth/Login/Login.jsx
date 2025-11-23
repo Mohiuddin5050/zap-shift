86cebcb9d31048910cf9499651d2484606f66c77
Mohiuddin5050 <kmmohiuddin5050@gmail.com>
2025-11-23 22:58:39 +0600

    created registration form using react hook form created login page and custom auth hook

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -24,7 +24,50 @@ const Login = () => {
   };
   return (
     <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
-     
+      <form className="card-body" onSubmit={handleSubmit(handleLogin)}>
+        <h3 className="text-3xl font-bold pt-5">Welcome Back</h3>
+        <p className="font-semibold">Login with ZapShift</p>
+        {/* email field */}
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is required</p>
+          )}
+          {/* password field */}
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password", { required: true, minLength: 6 })}
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500">Password is required</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500">
+              Password must be 6 characters or longer
+            </p>
+          )}
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-primary text-black mt-4">Login</button>
+        </fieldset>
+        <p>
+          Don’t have any account?{" "}
+          <Link className="text-blue-900 underline" to="/register">
+            Register
+          </Link>
+        </p>
+      </form>
+      <SocialLogin />
     </div>
   );
 };
--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -23,7 +23,57 @@ const Register = () => {
   };
   return (
     <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
-     
+      <form className="card-body" onSubmit={handleSubmit(handleRegister)}>
+        <h3 className="text-3xl font-bold pt-5">Create an Account</h3>
+        <p className="font-semibold">Register with ZapShift</p>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is required</p>
+          )}
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: true,
+              minLength: 6,
+              pattern:
+                /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+            })}
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500">Password is required</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500">
+              Password must be 6 characters or longer
+            </p>
+          )}
+          {errors.password?.type === "pattern" && (
+            <p className="text-red-500">
+              Password must contain at least 1 uppercase, 1 lowercase, 1 number,
+              and 1 special character.
+            </p>
+          )}
+
+          <button className="btn btn-primary text-black mt-4">Register</button>
+        </fieldset>
+        <p>
+          Already have an account?{" "}
+          <Link className="text-blue-900 underline" to="/login">
+            Login
+          </Link>
+        </p>
+      </form>
+      <SocialLogin />
     </div>
   );
 };
