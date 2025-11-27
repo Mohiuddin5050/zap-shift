926e98a5af6d8629a3f320a71a853768bb4bc47f
Mohiuddin5050 <kmmohiuddin5050@gmail.com>
2025-11-27 23:42:45 +0600

    added payment page

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -10,6 +10,7 @@ import Rider from "../pages/Rider/Rider";
 import SendParcel from "../pages/sendParcel/sendParcel";
 import DashBoardLayout from "../layouts/DashBoardLayout";
 import MyParcels from "../pages/Dashboard/MyParcels/MyParcels";
+import Payment from "../pages/Dashboard/Payment/Payment";
 
 export const router = createBrowserRouter([
   {
@@ -69,6 +70,10 @@ export const router = createBrowserRouter([
       {
         path: "myParcels",
         Component: MyParcels,
+      },
+      {
+        path: "payment/:parcelId",
+        Component: Payment,
       },
     ],
   },
